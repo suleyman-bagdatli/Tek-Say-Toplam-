8f0c452e045d0878b18f30225a3a0fbc3bae63f8
suleyman-bagdatli <suleymanbagdatli28@gmail.com>
2026-06-09 14:54:25 +0300

    Add files via upload

---
+++ Tek Sayı Toplamı.cs
@@ -0,0 +1,32 @@
+﻿namespace PROJE4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int Sonuc = 0;
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        Sonuc += i;
+                    }
+                }
+
+                Console.WriteLine("1 ile 10 arasındaki tek sayıların toplamı {0}'dır.", Sonuc);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
